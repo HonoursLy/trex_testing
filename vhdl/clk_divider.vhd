bf33b741d2f2cccad1b8ed40552bdfb6e377fd6b
Cassandra Ly <cass@cass.com>
2025-10-04 14:53:57 +0800

    toggle clock to change clock output

---
+++ vhdl/clk_divider.vhd
@@ -0,0 +1,36 @@
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.numeric_std.all;
+
+ENTITY clk_divider IS
+    GENERIC (
+        Freq_in : INTEGER := 12500000,
+        N : INTEGER := 2); -- 50MHz clock on DE-10 Lite FPGA board
+    PORT (
+        clk_in : IN STD_LOGIC;
+        reset : IN STD_LOGIC;
+        clk_out : OUT STD_LOGIC
+
+    );
+END clk_divider;
+
+ARCHITECTURE Behav OF clk_divider IS
+    SIGNAL temp : STD_LOGIC;
+    SIGNAL counter : INTEGER;
+BEGIN
+    frequency_divider : PROCESS (reset, clk_in)
+    BEGIN
+        IF (reset = '0') THEN
+            temp <= '0';
+            counter <= 0;
+        ELSIF rising_edge(clk_in) THEN
+            IF (counter > Freq_in/N) THEN
+                temp <= NOT(temp);
+                counter <= 0;
+            ELSE
+                counter <= counter + 1;
+            END IF;
+        END IF;
+    END PROCESS;
+    clk_out <= temp;
+END Behav;
--- vhdl/top.vhd
+++ vhdl/top.vhd
@@ -1,51 +1,140 @@
+-- Clock generator 1.5, 3MHz, 4.5MHz, 12MHz, 24MHz, 48MHz, 70MHz, 100Mhz
 library ieee;
 use ieee.std_logic_1164.all;
 
 entity top is
 	port (
-		a_i : in std_logic;
-		b_i : in std_logic;
-		c_i : in std_logic;
+		switched : in STD_LOGIC_VECTOR (2 downto 0);
+		reset : in STD_LOGIC;
+		clock_100 : in std_logic;
 		d_o : out std_logic
 	);
 end top;
 
 architecture rtl of top is
-	component and2 
-		port (
-			a : in std_logic;
-			b : in std_logic;
-			y : out std_logic
-		);
-	end component;
 
-	component or2 
-		port (
-			a : in std_logic;
-			b : in std_logic;
-			y : out std_logic
-		);
-	end component;
+component clk_divider IS
+    GENERIC (
+        Freq_in : INTEGER := 12500000,
+        N : INTEGER := 2); -- 50MHz clock on DE-10 Lite FPGA board
+    PORT (
+        clk_in : IN STD_LOGIC;
+        reset : IN STD_LOGIC;
+        clk_out : OUT STD_LOGIC
+    );
+end component;
+SIGNAL clk_15 : STD_LOGIC;
+SIGNAL clk_3 : STD_LOGIC;
+SIGNAL clk_45 : STD_LOGIC;
+SIGNAL clk_12 : STD_LOGIC;
+SIGNAL clk_24 : STD_LOGIC;
+SIGNAL clk_48 : STD_LOGIC;
+SIGNAL clk_70 : STD_LOGIC;
 
-	signal and2_o : std_logic;
-	signal or2_o : std_logic;
 
 begin
 
-	and_inst : and2
+	clk15 : clk_divider
+	generic map (
+		Freq_in => 100000000,
+		N => 3
+	),
 	port map (
-		a => a_i, 
-		b => b_i,
-		y => and2_o
+		clk_in =>clock_100,
+		reset => reset,
+		clk_out => clk_15
+
 	);
 
-	or_inst : or2
+	clk3 : clk_divider
+	generic map (
+		Freq_in => 100000000,
+		N => 6
+	),
 	port map (
-		a => and2_o,
-		b => c_i,
-		y => or2_o
+		clk_in =>clock_100,
+		reset => reset,
+		clk_out => clk_3
+
 	);
-	
-	d_o <= or2_o;
+
+	clk45 : clk_divider
+	generic map (
+		Freq_in => 100000000,
+		N => 9
+	),
+	port map (
+		clk_in =>clock_100,
+		reset => reset,
+		clk_out => clk_45
+
+	);
+	clk12 : clk_divider
+	generic map (
+		Freq_in => 100000000,
+		N => 24
+	),
+	port map (
+		clk_in =>clock_100,
+		reset => reset,
+		clk_out => clk_12
+
+	);
+
+	clk24 : clk_divider
+	generic map (
+		Freq_in => 100000000,
+		N => 48
+	),
+	port map (
+		clk_in =>clock_100,
+		reset => reset,
+		clk_out => clk_24
+
+	);
+
+	clk48 : clk_divider
+	generic map (
+		Freq_in => 100000000,
+		N => 96
+	),
+	port map (
+		clk_in =>clock_100,
+		reset => reset,
+		clk_out => clk_48
+
+	);
+
+	clk70 : clk_divider
+	generic map (
+		Freq_in => 100000000,
+		N => 140
+	),
+	port map (
+		clk_in =>clock_100,
+		reset => reset,
+		clk_out => clk_70
+
+	);
+
+	process (clock_100)
+	begin	
+		case switched is
+			when "000" =>
+				d_o <= clk_15;
+			when "001" =>
+				d_o <= clk_3;
+			when "011" =>
+				d_o <= clk_45;
+			when "010" =>
+				d_o <= clk_12;
+			when "110" =>
+				d_o <= clk_24;
+			when "111" =>
+				d_o <= clk_48;
+			when "101" =>
+				d_o <= clk_70;
+			when "100" =>
+				d_o <= clock_100;
 
 end architecture;
